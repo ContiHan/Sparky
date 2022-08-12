c3e7731f3371c2efe7983157452280da9cdb2659
contihan <daniel.hanak@continental-corporation.com>
2022-08-12 07:30:48 +0200

    advance string tests

--- SparkyNUnitTest/CustomerNUnitTests.cs
+++ SparkyNUnitTest/CustomerNUnitTests.cs
@@ -19,8 +19,16 @@ namespace Sparky
             // Act
             string greetCombinedName = customer.GreetAndCombine("Han", "Solo");
 
+
+
             // Assert
-            Assert.That(greetCombinedName, Is.EqualTo("Hello, Han Solo"));
+            Assert.AreEqual("Hello, Han Solo", greetCombinedName); // classic model
+            Assert.That(greetCombinedName, Is.EqualTo("Hello, Han Solo")); // constraint model
+            Assert.That(greetCombinedName, Does.Contain(",")); // contain
+            Assert.That(greetCombinedName, Does.StartWith("Hello")); // start with
+            Assert.That(greetCombinedName, Does.EndWith("Solo")); // end with
+            Assert.That(greetCombinedName, Does.Contain("han solo").IgnoreCase); // ignore case sensitive
+            Assert.That(greetCombinedName, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+")); // regular expression / string pattern match
         }
     }
 }
