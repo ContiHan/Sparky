59945979b4451a37733c987ba3b4ad02b33f6429
contihan <daniel.hanak@continental-corporation.com>
2022-08-12 07:07:41 +0200

    NUnit and string test

---
+++ SparkyNUnitTest/CustomerNUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class CustomerNUnitTests
+    {
+        [Test]
+        public void CombineName_InputFirstAndLastName_ReturnGreetedCombinedName()
+        {
+            // Arrange
+            Customer customer = new();
+
+            // Act
+            string greetCombinedName = customer.GreetAndCombine("Han", "Solo");
+
+            // Assert
+            Assert.That(greetCombinedName, Is.EqualTo("Hello, Han Solo"));
+        }
+    }
+}
