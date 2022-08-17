ec0a3f94754246e3e45ab4d40117b09c1564f513
ContiHan <daniel.hanak@continental-corporation.com>
2022-08-17 13:04:24 +0200

    fibo test class

---
+++ SparkyNUnitTest/FiboNUnitTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class FiboNUnitTests
+    {
+        private Fibo fibo;
+
+        [SetUp]
+        public void Setup()
+        {
+            fibo = new Fibo();
+        }
+
+        [Test]
+        public void FiboSequence_InputRange1_GetRightResult() // 0
+        {
+            // Arrange
+            var fiboExpectedRange = new List<int>() { 0 };
+
+            // Act
+            fibo.Range = 1;
+            var fiboResult = fibo.GetFiboSeries();
+
+            // Assert
+            Assert.That(fiboResult, Is.Not.Empty);
+            Assert.That(fiboResult, Is.Ordered);
+            Assert.That(fiboResult, Is.EquivalentTo(fiboExpectedRange));
+        }
+
+        [Test]
+        public void FiboSequence_InputRange6_GetRightResult() // 0, 1, 1, 2, 3, 5
+        {
+            // Arrange
+            var fiboExpectedRange = new List<int>() { 0, 1, 1, 2, 3, 5 };
+
+            // Act
+            fibo.Range = 6;
+            var fiboResult = fibo.GetFiboSeries();
+
+            // Assert
+            Assert.That(fiboResult, Has.Member(3));
+            Assert.That(fiboResult, Does.Not.Contain(4));
+            Assert.That(fiboResult, Has.Count.EqualTo(6));
+            Assert.That(fiboResult, Is.EquivalentTo(fiboExpectedRange));
+        }
+    }
+}
