d2a67f88bc7f1d7ca460bed3b17a79c57e0e2b60
ContiHan <daniel.hanak@continental-corporation.com>
2022-08-17 16:23:54 +0200

    mock in action

--- SparkyNUnitTest/BankAccountNUnitTests.cs
+++ SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,12 +16,25 @@ namespace Sparky
         [SetUp]
         public void Setup()
         {
+        }
+
+        [Test]
+        public void BankDepositLogFakker_Add100_ReturnTrue()
+        {
             bankAccount = new(new LogFakker());
+
+            var result = bankAccount.Deposit(100);
+
+            Assert.That(result, Is.True);
+            Assert.That(bankAccount.GetBalance(), Is.EqualTo(100));
         }
 
         [Test]
         public void BankDeposit_Add100_ReturnTrue()
         {
+            var logMock = new Mock<ILogBook>();
+            bankAccount = new(logMock.Object);
+
             var result = bankAccount.Deposit(100);
 
             Assert.That(result, Is.True);
