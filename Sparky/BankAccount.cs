05c16fdd0fa226b09deeed6a1fe82f69d0263a2b
ContiHan <daniel.hanak@continental-corporation.com>
2022-08-17 14:42:17 +0200

    BankAccount class

---
+++ Sparky/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    public class BankAccount
+    {
+        public int Balance { get; set; }
+
+        public BankAccount()
+        {
+            Balance = 0;
+        }
+
+        public bool Deposit(int amount)
+        {
+            Balance += amount;
+            return true;
+        }
+
+        public bool Withdraw(int amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+                return true;
+            }
+            return false;
+        }
+
+        public int GetBalance()
+        {
+            return Balance;
+        }
+    }
+}
