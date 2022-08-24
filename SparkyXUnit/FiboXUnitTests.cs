bb566e79c1e21e5d6f55cbe74dce42a114289d63
ContiHan <daniel.hanak@continental-corporation.com>
2022-08-24 14:40:56 +0200

    fibo xunit tests

--- SparkyXUnit/FiboXUnitTests.cs
+++ SparkyXUnit/FiboXUnitTests.cs
@@ -1,47 +1,48 @@
-﻿//namespace Sparky
-//{
-//    [TestFixture]
-//    public class FiboXUnitTests
-//    {
-//        private Fibo fibo;
+﻿using Xunit;
 
-//        [SetUp]
-//        public void Setup()
-//        {
-//            fibo = new Fibo();
-//        }
+namespace Sparky
+{
+    public class FiboXUnitTests
+    {
+        private readonly Fibo fibo;
 
-//        [Test]
-//        public void FiboSequence_InputRange1_GetRightResult() // 0
-//        {
-//            // Arrange
-//            var fiboExpectedRange = new List<int>() { 0 };
+        public FiboXUnitTests()
+        {
+            fibo = new Fibo();
+        }
 
-//            // Act
-//            fibo.Range = 1;
-//            var fiboResult = fibo.GetFiboSeries();
+        [Fact]
+        public void FiboSequence_InputRange1_GetRightResult() // 0
+        {
+            // Arrange
+            var fiboExpectedRange = new List<int>() { 0 };
 
-//            // Assert
-//            Assert.That(fiboResult, Is.Not.Empty);
-//            Assert.That(fiboResult, Is.Ordered);
-//            Assert.That(fiboResult, Is.EquivalentTo(fiboExpectedRange));
-//        }
+            // Act
+            fibo.Range = 1;
+            var fiboResult = fibo.GetFiboSeries();
 
-//        [Test]
-//        public void FiboSequence_InputRange6_GetRightResult() // 0, 1, 1, 2, 3, 5
-//        {
-//            // Arrange
-//            var fiboExpectedRange = new List<int>() { 0, 1, 1, 2, 3, 5 };
+            // Assert
+            Assert.NotEmpty(fiboResult);
+            Assert.Equal(fiboResult.OrderBy(f => f), fiboResult);
+            Assert.Equal(fiboExpectedRange, fiboResult);
+            Assert.True(fiboResult.SequenceEqual(fiboExpectedRange));
+        }
 
-//            // Act
-//            fibo.Range = 6;
-//            var fiboResult = fibo.GetFiboSeries();
+        [Fact]
+        public void FiboSequence_InputRange6_GetRightResult() // 0, 1, 1, 2, 3, 5
+        {
+            // Arrange
+            var fiboExpectedRange = new List<int>() { 0, 1, 1, 2, 3, 5 };
 
-//            // Assert
-//            Assert.That(fiboResult, Has.Member(3));
-//            Assert.That(fiboResult, Does.Not.Contain(4));
-//            Assert.That(fiboResult, Has.Count.EqualTo(6));
-//            Assert.That(fiboResult, Is.EquivalentTo(fiboExpectedRange));
-//        }
-//    }
-//}
+            // Act
+            fibo.Range = 6;
+            var fiboResult = fibo.GetFiboSeries();
+
+            // Assert
+            Assert.Contains(3, fiboResult);
+            Assert.DoesNotContain(4, fiboResult);
+            Assert.Equal(6, fiboResult.Count);
+            Assert.Equal(fiboExpectedRange, fiboResult);
+        }
+    }
+}
